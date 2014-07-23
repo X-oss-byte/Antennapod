c2080b1dfd17fc371e04ce1e7b39ebadaf3cb7a7
David Reiss <dreiss@fb.com>
2014-07-22 22:23:52 -0700

    Check in an empty FlattrConfig for easier setup

---
+++ src/de/danoeh/antennapod/util/flattr/FlattrConfig.java
@@ -0,0 +1,11 @@
+// The files and modifications provided by Facebook are for testing and evaluation purposes only.  Facebook reserves all rights not expressly granted.
+
+package de.danoeh.antennapod.util.flattr;
+
+
+/** Contains credentials to access the Flattr API*/
+public class FlattrConfig {
+	static final String APP_KEY = "";
+	static final String APP_SECRET = "";
+}
+
